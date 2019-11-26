0eafae866e3d594f8116cfd456e68ef4f9ad6195
Andrew Bernauer <bernaue2@illinois.edu>
2019-11-26 09:40:59 -0600

    finished stack test code for nontype template Stack<>

--- Templates_Complete_Guide/basics/stack4test.cpp
+++ Templates_Complete_Guide/basics/stack4test.cpp
@@ -8,3 +8,23 @@
 int main()
 {
   try{
+     Stack<int, 20> int20Stack; // stack up to 20 ints
+     Stack<int, 40> int40Stack; // stack up to 40 ints
+     Stack<std::string, 40> stringStack; // stack of up to 40 strings
+
+     // manipulate stack of up to 20 ints
+     int20Stack.push(7);
+     std::cout << stringStack.top() << std::endl;
+     int20Stack.pop();
+
+     // manipulate stack of up to 40 strings
+     stringStack.push("hello");
+     std::cout << stringStack.top() << std::endl;
+     stringStack.pop();
+     stringStack.pop();
+  }
+  catch (std::exception const& ex) {
+    std::cerr << "Exception: " << ex.what() << std::endl;
+    return EXIT_FAILURE; // exit program with ERROR status
+  }
+}
