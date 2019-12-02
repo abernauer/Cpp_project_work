d7f7334ef98ff46bf5c7fe68f6f565d7bc336f36
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-02 11:36:27 -0600

    finished function def to print elements of STL container

--- Templates_Complete_Guide/basics/printcoll.hpp
+++ Templates_Complete_Guide/basics/printcoll.hpp
@@ -7,4 +7,11 @@
 template <typename T>
 void printcoll (T const& call)
 {
-  
+  typename T::const_iterator pos; // iterator to iterate over coll
+  typename T::const_iterator end(coll.end()); // end position
+
+  for (pos=coll.begin(); pos!=end; ++pos) {
+    std::cout << *pos << ' ';
+  }
+  std::cout << endl;
+}
