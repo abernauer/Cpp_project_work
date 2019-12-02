1145ce8bd260106b2950b612754687b9313a306a
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-02 13:39:39 -0600

    Stack<> declaration with default paramater

---
+++ Templates_Complete_Guide/basics/stack6decl.hpp
@@ -0,0 +1,19 @@
+// stack6decl.hpp
+
+template <typename T, typename CONT = std::deque<T> >
+class Stack{
+  private :
+  CONT elems; //elements
+
+  public :
+  void push(T const&); //push element
+  void pop(); //pop element
+  T top() const; // return top element
+  bool empty() const { // return whether the stack is empty
+    return elems.empty();
+  }
+
+  // assign stack of elements of type T2
+  template <typename T2, typename CONT2>
+  stack<T,CONT>& operator= (Stack<T2,CONT2> const&);
+};
