5a36a725a794bfb765d9a0571af2c146b02a47ad
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-03 14:55:58 -0600

    function template passing string literal to ref and nonref

---
+++ Templates_Complete_Guide/basics/refnonref.cpp
@@ -0,0 +1,24 @@
+// refnonref.cpp
+
+#include <typeinfo>
+#include <iostream>
+
+template <typename T>
+void ref (T const& x)
+{
+  std::cout << "x in ref(T const&): "
+            << typeid(x).name() << '\n';
+}
+
+template <typename T>
+void nonref (T x)
+{
+  std::cout << "x in nonref(T):    "
+            << typeid(x).name() << '\n';
+}
+
+int main()
+{
+  ref("hello");
+  nonref("hello");
+}
