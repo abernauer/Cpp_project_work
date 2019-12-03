f330eddb077bf3c300b3d07bd531d0edc48e3f29
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-03 13:56:29 -0600

    max function template demonstrating decay

---
+++ Templates_Complete_Guide/basics/max6.cpp
@@ -0,0 +1,20 @@
+// max6.cpp
+
+#include <string>
+
+// nonreference parameters
+template <typename T>
+inline T max (T a, T b,)
+{
+  return a < b ? b : a;
+}
+
+int main()
+{
+  std::string s;
+
+  ::max("apple", "peach"); // OK: same type
+  ::max("apple", "tomato"); // OK: decays to same type
+  ::max("apple", s); // ERROR: different types
+}
+
