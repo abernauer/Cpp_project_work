3194f18463a85af6774c277d84672806727f63ea
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-03 17:09:29 -0600

    illustrating template linker errors

---
+++ Templates_Complete_Guide/basics/myfirstmain.cpp
@@ -0,0 +1,11 @@
+// myfirstmain.cpp
+
+#include "myfirst.hpp"
+
+//use of the template
+int main()
+{
+  double ice = 3.0;
+  print_typeof(ice); // call function template for type double
+  // demonstrates linker errors
+}
