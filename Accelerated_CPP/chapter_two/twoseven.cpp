f4def8ebbb1e324285794bd1a2d42492b8f2fc79
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-11 14:53:50 -0600

    counts down from 10 to -5

---
+++ Accelerated_CPP/chapter_two/twoseven.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using std::cout; using std::endl;
+
+
+int main()
+{
+  // intialize j to 10 
+  int j = 10;
+
+  while (j >= -5) {
+    cout << j << endl;
+    j -= 1;
+   
+  }
+
+}
