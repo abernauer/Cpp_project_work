3a314f34480cbd1713ab1363b2b525dac8d39756
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-27 15:56:02 -0600

    prints 10 to 0 to standard out

---
+++ CPP_Primer/chapter_one/tentozero.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::cout; using std::endl;
+
+int main()
+{
+  int val = 10, count = 0;
+  
+  while (val >= count) {
+    cout << val << endl;
+    --val;
+  }
+  return 0;
+}
