364d0e1d9d4572daa137966d55a70d0f828c6205
Andrew Bernauer <bernaue2@illinois.edu>
2019-12-28 15:09:56 -0600

    range less than or equal to inclusive

---
+++ CPP_Primer/chapter_one/rangeTwo.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::endl; using std::cout;
+using std::cin;
+
+int main ()
+{
+
+  cout << "Please enter two integers: " << endl;
+  int v1 = 0, v2 = 0;
+  cin >> v1;
+  cin >> v2;
+
+  while ( v2 <= v1 ) {
+    cout << v2 << endl;
+    ++v2;
+  }
+
+  return 0;
+
+}
